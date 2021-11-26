46102dfb13e578d8737b5de2ad76e457d5eaffaa
Nicholas Shephard <shephardn@gmail.com>
2021-11-26 12:00:28 -0600

    commit tree 6727eea8e93b8ce1b69c29e211f0dbc7aa9be55f

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+ const [isAdded, cartToggle] = useState(false);
+//  console.log(isAdded);
+
+ const cartState = isAdded ? "Remove From Cart" : "Add to Cart";
+ const cartClass = isAdded ? "in-cart" : "";
+
   return (
-    <li className="">
+    <li className={cartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={() => cartToggle(!isAdded)}>{cartState}</button>
     </li>
   );
 }
